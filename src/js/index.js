7930a7df77bc84132c841e1e31311c4deeda22d8
DESKTOP-3P213DA\sakibur <gmsakibursabbir@gmail.com>
2025-04-12 17:04:05 +0600

    upadted

--- src/js/index.js
+++ src/js/index.js
@@ -165,11 +165,16 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 //smooth-scroll
-document.querySelector('a[href="#about"]').addEventListener("click", (e) => {
-  e.preventDefault();
-  document.querySelector("#about").scrollIntoView({ behavior: "smooth" });
-});
-document.querySelector('a[href="#contact"]').addEventListener("click", (e) => {
-  e.preventDefault();
-  document.querySelector("#contact").scrollIntoView({ behavior: "smooth" });
+const smoothScrollLinks = document.querySelectorAll('a[href^="#"]');
+
+smoothScrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const targetId = link.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  });
 });
