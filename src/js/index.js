c75d89f29579bde87c646e1f64791edd6eacde68
DESKTOP-3P213DA\sakibur <gmsakibursabbir@gmail.com>
2025-04-12 16:56:33 +0600

    updated

--- src/js/index.js
+++ src/js/index.js
@@ -163,3 +163,13 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+//smooth-scroll
+document.querySelector('a[href="#about"]').addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector("#about").scrollIntoView({ behavior: "smooth" });
+});
+document.querySelector('a[href="#contact"]').addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector("#contact").scrollIntoView({ behavior: "smooth" });
+});
